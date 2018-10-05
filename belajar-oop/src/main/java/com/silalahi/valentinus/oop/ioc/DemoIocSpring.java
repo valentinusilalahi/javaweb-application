f1acb05485da16c78f28d591001d9609d4edc6d2
valentinus silalahi <valentinus.silalahi@gmail.com>
2018-10-05 14:23:17 +0700

    inversion of control (IoC) menggunakan spring 4.3.10

---
+++ belajar-oop/src/main/java/com/silalahi/valentinus/oop/ioc/DemoIocSpring.java
@@ -0,0 +1,12 @@
+package com.silalahi.valentinus.oop.ioc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DemoIocSpring {
+	public static void main(String[] xxx) {
+		ApplicationContext springContainer = new ClassPathXmlApplicationContext("training-ioc.xml");
+		NasabahDaoIoc nasabahDao = (NasabahDaoIoc) springContainer.getBean("nd");
+		nasabahDao.simpan();
+	}
+}
---
+++ belajar-oop/src/main/java/com/silalahi/valentinus/oop/ioc/NasabahDaoIoc.java
@@ -0,0 +1,33 @@
+package com.silalahi.valentinus.oop.ioc;
+
+public class NasabahDaoIoc {
+	// tidak ada inisialisasi, karena sudah di handle di tempat laen
+	private KoneksiDatabase koneksiDatabase;
+
+	public NasabahDaoIoc(KoneksiDatabase koneksiDatabase) {
+		this.koneksiDatabase = koneksiDatabase;
+	}
+
+	// setter injection
+	public void setKoneksiDatabase(KoneksiDatabase koneksiDatabase) {
+		this.koneksiDatabase = koneksiDatabase;
+	}
+
+	private void connect() {
+		koneksiDatabase.connect();
+	}
+
+	private void disconnect() {
+
+	}
+
+	public void simpan() {
+		connect();
+		System.out.println("Menyimpan data");
+		disconnect();
+	}
+
+	public void cariData() {
+
+	}
+}
